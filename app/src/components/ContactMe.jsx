38cb49ffd2e175769b0d1ac10502df7b7807da4a
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-08-21 09:50:42 -0400

    - Change structure - Add Routes and Links - etc

---
+++ app/src/components/ContactMe.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ContactMe() {
+    return (
+        <div>
+            <h1>Contact Me</h1>
+
+            <p>Email</p>
+            <p>LinkedIn</p>
+            <p>GitHub</p>
+            <p>Resume</p>
+        </div>
+    )
+}
