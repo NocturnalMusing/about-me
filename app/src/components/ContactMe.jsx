55518d49e1c1b16684ca2646d5e03a013a28d5ca
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-08-21 09:54:01 -0400

    Add Link and <a> tags to Contact Me

--- app/src/components/ContactMe.jsx
+++ app/src/components/ContactMe.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 export default function ContactMe() {
     return (
         <div>
             <h1>Contact Me</h1>
 
             <p>Email</p>
-            <p>LinkedIn</p>
-            <p>GitHub</p>
-            <p>Resume</p>
+            <a href='https://www.linkedin.com/in/calliope-jimenez-clark/'>
+                LinkedIn
+            </a>
+            <a href='https://github.com/NocturnalMusing'>
+                GitHub
+            </a>
+            <Link to='/resume'>
+                Resume
+            </Link>
         </div>
     )
 }
