edde63123dba87c185af0b1278e033eef7fad98b
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-08-23 12:12:13 -0400

    Clear dead code

--- app/src/components/Portfolio.jsx
+++ app/src/components/Portfolio.jsx
@@ -6,14 +6,6 @@ import { cards } from '../assets/portfolio_cards'
 
 export default function Portfolio() {
 
-    // const showSlide = (num) => {
-    //     for (let i = 0; i < cards.length; i++) {
-    //         cards[i].setAttribute('style', 'display: none;')
-    //     }
-
-    //     cards[num].setAttribute('style', 'display: block;')
-    // }
-
     let current = 0
 
     const handleClickRight = (e) => {
@@ -51,57 +43,3 @@ export default function Portfolio() {
         </div>
     )
 }
-
-
-{/* <Carousel interval={null} className='carousel'>
-                <Carousel.Item className='carousel-item'>
-                    <img
-                        className="d-block w-100"
-                        src="https://img.cinemablend.com/filter:scale/quill/1/4/e/5/2/1/14e5214b7130f782d5ae28042bc88082fa56d8b3.jpg?mw=600"
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-
-                <Carousel.Item className='carousel-item'>
-                    <img
-                        className="d-block w-100"
-                        src="https://img.cinemablend.com/filter:scale/quill/1/4/e/5/2/1/14e5214b7130f782d5ae28042bc88082fa56d8b3.jpg?mw=600"
-                        alt="Second slide"
-                    />
-
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                
-                <Carousel.Item className='carousel-item'>
-                    <img
-                        className="d-block w-100"
-                        src="https://img.cinemablend.com/filter:scale/quill/1/4/e/5/2/1/14e5214b7130f782d5ae28042bc88082fa56d8b3.jpg?mw=600"
-                        alt="Third slide"
-                    />
-
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-
-                <Carousel.Item className='carousel-item'>
-                    <img
-                        className="d-block w-100"
-                        src="https://img.cinemablend.com/filter:scale/quill/1/4/e/5/2/1/14e5214b7130f782d5ae28042bc88082fa56d8b3.jpg?mw=600"
-                        alt="Fourth slide"
-                    />
-
-                    <Carousel.Caption>
-                        <h3>Fourth slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel> */}
