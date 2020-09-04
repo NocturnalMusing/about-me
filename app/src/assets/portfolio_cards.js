f6b92d3b76d5b889a67e69db4c8701b5c36866c2
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-09-04 15:05:10 -0400

    Begin basic styling, center carousel

--- app/src/assets/portfolio_cards.js
+++ app/src/assets/portfolio_cards.js
@@ -1,8 +1,8 @@
 export const cards = [
     {
         "img": "https://img.cinemablend.com/filter:scale/quill/0/1/9/b/1/e/019b1e6d12a6a63b3cdafe1b95a34734b136fdd9.jpg?fw=1200",
-        "alt": "...",
-        "name": "one",
+        "alt": "Pixel Menagerie Homepage",
+        "name": "Pixel Menagerie",
         "caption": "one",
         "url": "/"
     },
--- app/src/components/Portfolio.jsx
+++ app/src/components/Portfolio.jsx
@@ -6,28 +6,32 @@ import { cards } from '../assets/portfolio_cards'
 
 export default function Portfolio() {
 
-    let [ count, setCount ] = useState(0)
+    let [count, setCount] = useState(0)
 
     const handleClickRight = (e) => {
         count < cards.length - 1 ? setCount(count + 1) : setCount(0)
     }
-    
+
     const handleClickLeft = (e) => {
         count > 0 ? setCount(count - 1) : setCount(cards.length - 1)
     }
-    
+
 
     return (
         <div className="carousel">
             <button onClick={handleClickLeft}>Back</button>
-            {cards.map(card =>
-                <div className={cards.indexOf(card) === count ? 'show' : 'hide'}>
-                    <img src={card.img} alt={card.alt}/>
-                    <h3>{card.name}</h3>
-                    <p>{card.caption}</p>
-                    <a href={card.url}>Live View</a>
-                </div>
-            )}
+            <div className='carousel-content'>
+                {cards.map(card =>
+                    <div className={cards.indexOf(card) === count ? 'show' : 'hide'}>
+                        <img className='carousel-img' src={card.img} alt={card.alt} />
+                        <div className='carousel-text'>
+                            <h3>{card.name}</h3>
+                            <p>{card.caption}</p>
+                            <a href={card.url}>Live View</a>
+                        </div>
+                    </div>
+                )}
+            </div>
             <button onClick={handleClickRight}>Forward</button>
         </div>
     )
