23bd5fa25076211d187963371044c2544883567a
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-09-07 11:18:20 -0400

    - Add <div> to MainPage - Add flex

--- app/src/screens/MainPage.jsx
+++ app/src/screens/MainPage.jsx
@@ -4,14 +4,16 @@ import AboutMe from '../components/AboutMe'
 import Portfolio from '../components/Portfolio'
 import ContactMe from '../components/ContactMe'
 
+import './MainPage.css'
+
 export default function MainPage() {
     return (
-        <>
+        <div className='main'>
             <AboutMe />
 
             <Portfolio />
 
             <ContactMe />
-        </>
+        </div>
     )
 }
