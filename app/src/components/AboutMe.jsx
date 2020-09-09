9adb838448c92201f7b97413752431086a7d83b6
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-09-09 13:28:48 -0400

    Add Poppins font to AboutMe

--- app/src/components/AboutMe.jsx
+++ app/src/components/AboutMe.jsx
@@ -8,10 +8,10 @@ import './AboutMe.css'
 export default function AboutMe() {
     return (
         <div className='about-me'>
-            <h1 className='first-name'>Calliope</h1>
-            <h2 className='last-name'>Jimenez-Clark</h2>
+            <h2 className='first-name'>Calliope</h2>
+            <h1 className='last-name'>Jimenez-Clark</h1>
 
-            <p>As a Front-End Web Developer and Visual Designer I leverage my creativity as an asset
+            <p className='blurb'>As a Front-End Web Developer and Visual Designer I leverage my creativity as an asset
                 towards my passion for building great products with a strong focus on aesthetics and functionality.</p>
 
             <FontAwesomeIcon icon={faCrow} size='2x' className='poe'/>
