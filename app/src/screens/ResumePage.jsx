2c97b478bb526f56b971db63e56a6bb34c5ccfda
Nocturnal <NocturnalMusing@users.noreply.github.com>
2022-05-11 11:48:34 -0400

    Add Resume_2022.png to ResumePage Add styling to center & size adjust Resume_2022.png

--- app/src/screens/ResumePage.jsx
+++ app/src/screens/ResumePage.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Resume from '../assets/images/Resume_2022.png'
+
+import './ResumePage.css'
 
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faAngleDoubleLeft } from '@fortawesome/free-solid-svg-icons'
 
-import { Resume } from '../assets/images/Resume.png'
+// import ResumeEmployment from '../components/ResumeEmployment'
+// import ResumeSkills from '../components/ResumeSkills'
+// import ResumeEducation from '../components/ResumeEducation'
 
 library.add(faAngleDoubleLeft)
 
@@ -16,7 +21,8 @@ export default function ResumePage() {
                 <FontAwesomeIcon icon='angle-double-left' size='3x'/>
             </Link>
 
-            <img src="{Resume}">
+            <img  src={Resume} alt="CJC_Resume_2022" className='resume_img'></img>
+            
         </div>
     )
 }
